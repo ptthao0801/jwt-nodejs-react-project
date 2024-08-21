9d862044dcf715a12e3ce0693e28de86b4d4cb1a
ptthao0801 <ptthao0801@gmail.com>
2024-08-22 01:50:56 +0700

    hash password, get user list

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,11 +1,4 @@
-import mysql from 'mysql2';
-
-// Create the connection to database
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  database: 'jwt',
-});
+import userService from '../service/userService';
 
 const handleHelloWorld = (req,res) => {
     const name = 'Thao';
@@ -25,13 +18,8 @@ const handleCreateNewUser = (req,res) => {
     let password = req.body.password;
     let username = req.body.username;
 
-    connection.query(
-          'INSERT INTO users (email, password, username) VALUES (?, ?, ?)', [email, password, username],
-          function(err, results, fields) {
-            console.log(results); // results contains rows returned by server
-          }
-    );
-    
+    // userService.createNewUser(email, password, username)
+    userService.getUserList();
     return res.send('handleCreateNewUser')
 }
 module.exports = {
