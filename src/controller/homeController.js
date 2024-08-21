d4ac6b62e18ff42a03e1eca76d8ae4fc850db6eb
ptthao0801 <ptthao0801@gmail.com>
2024-08-22 01:11:03 +0700

    connect mysql, config body-parser, add names to sign-in form

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,3 +1,12 @@
+import mysql from 'mysql2';
+
+// Create the connection to database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'jwt',
+});
+
 const handleHelloWorld = (req,res) => {
     const name = 'Thao';
     return res.render('home.ejs', {name});
@@ -11,8 +20,23 @@ const handleUserPage = (req,res) => {
     return res.render('user.ejs');
 }
 
+const handleCreateNewUser = (req,res) => {
+    let email = req.body.email;
+    let password = req.body.password;
+    let username = req.body.username;
+
+    connection.query(
+          'INSERT INTO users (email, password, username) VALUES (?, ?, ?)', [email, password, username],
+          function(err, results, fields) {
+            console.log(results); // results contains rows returned by server
+          }
+    );
+    
+    return res.send('handleCreateNewUser')
+}
 module.exports = {
     handleAbout,
     handleHelloWorld,
-    handleUserPage
+    handleUserPage,
+    handleCreateNewUser
 }
