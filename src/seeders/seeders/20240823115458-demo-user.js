b97a998826d5ef744223fa4dc5b9527941fabfd5
ptthao0801 <ptthao0801@gmail.com>
2024-08-23 19:04:00 +0700

    generate migrations, generate model, fake data

---
+++ src/seeders/seeders/20240823115458-demo-user.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert('Users', [
+        {
+         email: 'John Doe',
+         password: '123',
+         username: 'fake11'
+        },
+        {
+          email: 'John Doe2',
+          password: '123',
+          username: 'fake2'
+         }
+      ], {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
